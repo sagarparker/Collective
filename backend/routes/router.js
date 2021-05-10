2ee5e611f684bec49fcc9da02c75e47d6a982e2a
sagarparker <sagar8parker@gmail.com>
2021-05-10 23:41:52 +0530

    Node.js Setup done

---
+++ backend/routes/router.js
@@ -0,0 +1,35 @@
+const express       =   require('express');
+const app           =   express();
+const cors          =   require("cors");
+
+
+//BODY PARSER PRESET
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.text({ limit: '200mb' }));
+
+
+// CORS PRESETS
+
+app.use(cors());
+
+app.use(express.static("docs"));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,PUT,OPTIONS');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
+
+
+module.exports = app;
