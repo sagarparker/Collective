f7b9ef7a163298fa08fab5f5c7d181eb2b33bd5d
sagarparker <sagar8parker@gmail.com>
2021-05-17 15:53:27 +0530

    Changes to the login screen UI

--- frontend/lib/screens/LoginScreen.dart
+++ frontend/lib/screens/LoginScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -5,90 +7,111 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/Logo.png',
-              height: 45,
-            ),
-            Text(
-              'Collective',
-              style: TextStyle(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 80,
+                padding: EdgeInsets.only(top: 20),
+                decoration: BoxDecoration(
                   color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 27),
-            ),
-          ],
-        ),
-        elevation: 1,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 245,
-              padding: const EdgeInsets.only(top: 50, left: 40, right: 40),
-              child: Form(
-                child: ListView(
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Email / Username',
-                      ),
+                    Image.asset(
+                      'assets/images/LogoCrop.png',
+                      fit: BoxFit.cover,
+                      height: 40,
+                      width: 40,
                     ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                      ),
-                      obscureText: true,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 25),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          primary: Theme.of(context).primaryColor,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          minimumSize: Size(double.infinity, 45),
-                        ),
-                        child: Text(
-                          'LOGIN',
-                          style: TextStyle(
+                    Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: Text(
+                        'Collective',
+                        style: TextStyle(
+                            color: Colors.white,
                             fontWeight: FontWeight.bold,
-                            fontSize: 17,
-                          ),
-                        ),
+                            fontSize: 30),
                       ),
                     )
                   ],
                 ),
               ),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                'New User? Register here.',
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 15,
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Theme.of(context).primaryColor,
+                      Colors.white,
+                    ],
+                  ),
+                ),
+                height: 290,
+                child: Image.asset(
+                  'assets/images/SignUpScreen.png',
+                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-            Container(
-              height: 330,
-              margin: EdgeInsets.only(
-                top: 40,
+              Container(
+                height: 250,
+                padding: const EdgeInsets.only(top: 35, left: 40, right: 40),
+                child: Form(
+                  child: ListView(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Email / Username',
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                        ),
+                        obscureText: true,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 25),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            elevation: 0,
+                            primary: Theme.of(context).primaryColor,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(60)),
+                            minimumSize: Size(double.infinity, 45),
+                          ),
+                          child: Text(
+                            'LOGIN',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
-              child: Image.asset(
-                'assets/images/LoginScreen.png',
-                fit: BoxFit.cover,
+              Container(
+                margin: EdgeInsets.only(top: 40),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'New User ? register here',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
