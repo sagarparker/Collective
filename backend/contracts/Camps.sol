933b8b96d4a2bbe7ba15e58de6c9337e87318c8b
sagarparker <sagar8parker@gmail.com>
2021-05-22 13:03:14 +0530

    Bug fix for funding in Camps.sol

--- backend/contracts/Camps.sol
+++ backend/contracts/Camps.sol
@@ -1,58 +1,77 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity ^0.8.0;
+
+// Camp - A camp that is created by a Creative ( OP - Original Posters ) on Collective to raise funding
+
+// Angel - Angels are the users who buy equity in exchange of CTV - CollectiveToken
 
 contract Camps {
     
-    mapping(string =>mapping(string => uint)) public donation;
+    mapping(string =>mapping(string => uint)) public funding;
     mapping(string => CampDetails) public camps;
     
     struct CampDetails{
-        bool isValue;
-        uint totalDonation;
-        string[] donorList;
-        string createdOn;
-        string expiresOn;
+        bool campExists;
+        uint fundingRaised;
+        string[] angelList;
         uint target;
         bool targetReached;
     }
     
+    
     //Create a new camp on Collective
     
-    function createCamp(string memory _camp, string memory _createdOn, string memory _expiresOn, uint _target) public {
-         camps[_camp].isValue = true;
-         camps[_camp].createdOn = _createdOn;
-         camps[_camp].expiresOn = _expiresOn;
+    function createCamp(string memory _camp, uint _target) public {
+         camps[_camp].campExists = true;
          camps[_camp].target = _target;
     }
     
+    
+    // Emit event when the camp target is reached
+    
+    event targetReachedForCamp(string _camp);
+    
+    
     // Buying equity in the camp
     
-    function makeDonation(string memory _donor,string memory _camp,uint _amount) public returns (string memory){
-        if(camps[_camp].isValue == true){
-            donation[_camp][_donor] = donation[_camp][_donor] + _amount;
-            camps[_camp].totalDonation = camps[_camp].totalDonation + _amount;
-            camps[_camp].donorList.push(_donor);
-            if(donation[_camp][_donor] + _amount >= camps[_camp].target){
-                camps[_camp].isValue = false;
-                camps[_camp].targetReached = true;
-            }
-            return 'Executed';
+    function buyEquity(string memory _angel,string memory _camp,uint _amount) public {
+        require(camps[_camp].campExists == true,'Camp not found');
+        if(camps[_camp].fundingRaised + _amount >= camps[_camp].target){
+            camps[_camp].campExists = false;
+            camps[_camp].targetReached = true;
         }
-        else{
-            return 'Camp does not exist';
+        funding[_camp][_angel] = funding[_camp][_angel] + _amount;
+        camps[_camp].fundingRaised = camps[_camp].fundingRaised + _amount;
+        camps[_camp].angelList.push(_angel);
+        
+
+        if(camps[_camp].targetReached){
+            emit targetReachedForCamp(_camp);
         }
         
     }
     
+    
     // get the total number of Angels who bought equity in a camp 
     
-    function getDonorListLength(string memory _camp) public view returns(uint){
-        return camps[_camp].donorList.length;
+    function getAngelListLength(string memory _camp) public view returns(uint){
+        return camps[_camp].angelList.length;
     }
+    
     
      // get the list of Angels who bought equity in a camp 
     
-    function getDonorList(string memory _camp) public view returns(string[] memory){
-        return camps[_camp].donorList;
+    function getAngelList(string memory _camp) public view returns(string[] memory){
+        return camps[_camp].angelList;
     }
+    
+    
+
 }
+
+
+
+
+
+
