9b7f5784658dbf4375a0ea69b73bff69c999f462
sagarparker <sagar8parker@gmail.com>
2021-10-17 20:03:25 +0530

    API testing

--- backend/routes/blockchain/CollectiveToken.js
+++ backend/routes/blockchain/CollectiveToken.js
@@ -7,16 +7,16 @@ require('dotenv').config();
 const collectiveTokenController = require('../../controllers/blockchainControllers/collectiveTokenController');
 
 
-// Get users account balance
+// // Get users account balance
 
-router.get('/getAccountBalance',
-    validateApiSecret,
-    body('account_address').not().isEmpty(),
-    collectiveTokenController.getAccountBalance
-);
+// router.get('/getAccountBalance',
+//     validateApiSecret,
+//     body('account_address').not().isEmpty(),
+//     collectiveTokenController.getAccountBalance
+// );
 
 
-// Get users account balance
+// // Get users account balance
 
 router.get('/getUsersAccountBalance',
     validateApiSecret,
@@ -25,58 +25,58 @@ router.get('/getUsersAccountBalance',
 );
 
 
-// Sending ETH to users
+// // Sending ETH to users
 
-router.post('/transferETH',
-    [body('transfer_address').not().isEmpty(),
-    body('amount').not().isEmpty()],
-    collectiveTokenController.transferETH
-);
-
-
-// Sending CTV - Collective token to users
-
-router.post('/transferCTV',
-    [body('transfer_address').not().isEmpty(),
-    body('amount').not().isEmpty()],
-    collectiveTokenController.transferCTV
-);
+// router.post('/transferETH',
+//     [body('transfer_address').not().isEmpty(),
+//     body('amount').not().isEmpty()],
+//     collectiveTokenController.transferETH
+// );
 
 
-// Transfer CTV to use - with AUTH
+// // Sending CTV - Collective token to users
 
-router.post('/transferCTVToUser',
-    validateApiSecret,
-    isAuthenticated,
-    body('amount').not().isEmpty(),
-    collectiveTokenController.transferCTVToUser
-);
-
-
-// Get the allowance for a account;
-
-router.post('/getAllowance',
-    [body('owner_address').not().isEmpty(),
-    body('spender_address').not().isEmpty()],
-    collectiveTokenController.getAllowance
-);
+// router.post('/transferCTV',
+//     [body('transfer_address').not().isEmpty(),
+//     body('amount').not().isEmpty()],
+//     collectiveTokenController.transferCTV
+// );
 
 
-// Transfer CTV between user accounts
+// // Transfer CTV to use - with AUTH
 
-/// How the API works
+// router.post('/transferCTVToUser',
+//     validateApiSecret,
+//     isAuthenticated,
+//     body('amount').not().isEmpty(),
+//     collectiveTokenController.transferCTVToUser
+// );
 
-// 1. Start with sending required ETH (gas) from the master account to CTV owner account
-// 2. Approve the transaction amount
-// 3. Send the amount to the transfer address from the CTV owner address
 
-router.post('/transferCTVbetweenUsers',
-    [body('owner_address').not().isEmpty(),
-    body('owner_private_key').not().isEmpty(),
-    body('transfer_address').not().isEmpty(),
-    body('amount').not().isEmpty()],
-    collectiveTokenController.transferCTVbetweenUsers
-);
+// // Get the allowance for a account;
+
+// router.post('/getAllowance',
+//     [body('owner_address').not().isEmpty(),
+//     body('spender_address').not().isEmpty()],
+//     collectiveTokenController.getAllowance
+// );
+
+
+// // Transfer CTV between user accounts
+
+// /// How the API works
+
+// // 1. Start with sending required ETH (gas) from the master account to CTV owner account
+// // 2. Approve the transaction amount
+// // 3. Send the amount to the transfer address from the CTV owner address
+
+// router.post('/transferCTVbetweenUsers',
+//     [body('owner_address').not().isEmpty(),
+//     body('owner_private_key').not().isEmpty(),
+//     body('transfer_address').not().isEmpty(),
+//     body('amount').not().isEmpty()],
+//     collectiveTokenController.transferCTVbetweenUsers
+// );
 
 
 module.exports = router;
