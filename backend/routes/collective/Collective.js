94f79d5e747e5bb214d587b2f17f10558673244d
sagarparker <sagar8parker@gmail.com>
2021-10-27 22:05:15 +0530

    Activating the APIs

--- backend/routes/collective/Collective.js
+++ backend/routes/collective/Collective.js
@@ -10,35 +10,35 @@ const collectiveController = require('../../controllers/collectiveControllers/co
 
 // Fetch user data
 
-// router.post('/getUserDetails',
-//   validateApiSecret,
-//   isAuthenticated,
-//   collectiveController.getUserDetails
-// );
+router.post('/getUserDetails',
+  validateApiSecret,
+  isAuthenticated,
+  collectiveController.getUserDetails
+);
 
 
-// // Withdraw camp amount raised
+// Withdraw camp amount raised
 
-// router.post('/withdrawAmount',
-//   validateApiSecret,
-//   isAuthenticated,
-//   [body('owner_address').not().isEmpty(),
-//   body('owner_private_key').not().isEmpty(),
-//   body('amount').not().isEmpty()],
-//   collectiveController.withdrawAmount
-// );
+router.post('/withdrawAmount',
+  validateApiSecret,
+  isAuthenticated,
+  [body('owner_address').not().isEmpty(),
+  body('owner_private_key').not().isEmpty(),
+  body('amount').not().isEmpty()],
+  collectiveController.withdrawAmount
+);
 
 
 
-// router.post('/supportEmail',
-//   validateApiSecret,
-//   isAuthenticated,
-//   [ 
-//     body('email_subject').not().isEmpty(),
-//     body('email_message').not().isEmpty()
-//   ],
-//   collectiveController.supportEmail
-// );
+router.post('/supportEmail',
+  validateApiSecret,
+  isAuthenticated,
+  [ 
+    body('email_subject').not().isEmpty(),
+    body('email_message').not().isEmpty()
+  ],
+  collectiveController.supportEmail
+);
 
 
 
